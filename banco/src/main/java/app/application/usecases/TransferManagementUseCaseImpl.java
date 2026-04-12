6c2e5f3fb0f20dea0a7bb8670f39b1e86e5946fd
Aldair Murillo <aldairmmm2@gmail.com>
2026-04-12 00:07:49 -0500

    CHG: Use EXECUTED status when transfer money movement is completed

--- banco/src/main/java/app/application/usecases/TransferManagementUseCaseImpl.java
+++ banco/src/main/java/app/application/usecases/TransferManagementUseCaseImpl.java
@@ -12,6 +12,8 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.*;
 
+// TODO: Delegate business logic to domain services instead of implementing directly here
+
 @Service
 @RequiredArgsConstructor
 public class TransferManagementUseCaseImpl implements TransferManagementUseCase {
@@ -85,7 +87,7 @@ public class TransferManagementUseCaseImpl implements TransferManagementUseCase 
         bankAccountPort.save(source);
         bankAccountPort.save(target);
 
-        t.setTransferStatus(TransferStatus.APPROVED);
+        t.setTransferStatus(TransferStatus.EXECUTED);
         t.setApprovalDate(LocalDateTime.now());
 
         Transfer updated = transferPort.save(t);
